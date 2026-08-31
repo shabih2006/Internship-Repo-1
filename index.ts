5b39ff9c582ba60c7682cc6e4336286a3ca31212
shabih2006 <shabih2471@gmailcom>
2026-08-31 13:01:20 +0500

    feat: add User interface and greetUser function

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 console.log("Hello! This is TypeScript!");
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function greetUser(user: User): string {
+  return `Hello, ${user.name}!`;
+}
+const sampleUser: User = { id: 1, name: "Shabih" };
+console.log(greetUser(sampleUser));
